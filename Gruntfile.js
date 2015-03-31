d313bbc2908c0b32345c1d46690aa0e69f9787bb
Vitaliy Kozyrev <vitaliy.kozyrev@wonga.com>
2015-03-31 19:19:44 +0300

    add grunt environment

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'controllers/**/*.js', 'modules/**/*.js', 'directives/**/*.js', 'services/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          keepalive: true,
+          onCreateServer: function(server, connect, options) {
+
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
